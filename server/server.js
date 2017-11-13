fbdca8ae375ea621fbc5341676d9c4df2c06252b
Ragulan <ragulan28@gmail.com>
2017-11-13 19:18:04 +0530

    add get/todos/ route

--- server/server.js
+++ server/server.js
@@ -19,6 +19,21 @@ app.post("/todos", (req, res) => {
         }
     );
 });
+
+app.get("/todos", (req, res) => {
+    Todo.find()
+        .then(
+            todos => {
+                re.send({
+                    todos
+                });
+            },
+            err => {
+                res.status(400).send(e);
+            }
+        );
+});
+
 app.listen(3000, () => {
     console.log("Start port 3000");
 });
