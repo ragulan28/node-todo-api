63ec6056c3cb1ec0dfd01dbe843bbfdde154379e
Ragulan <ragulan28@gmail.com>
2017-11-13 20:32:24 +0530

    Add get/todos:id

--- server/server.js
+++ server/server.js
@@ -3,35 +3,56 @@ var bodyParser = require("body-parser");
 var { mongoose } = require("./db/mongoose");
 var { Todo } = require("./models/todo");
 var { User } = require("./models/user");
+var { ObjectID } = require('mongodb');
 
 var app = express();
 app.use(bodyParser.json());
-app.post("/todos", (req, res) => {
-    var todo = new Todo({
-        text: req.body.text
-    });
-    todo.save().then(
-        doc => {
-            res.send(doc);
-        },
-        err => {
-            res.status(400).send(err);
-        }
-    );
-});
 
-app.get("/todos", (req, res) => {
-    Todo.find()
-        .then(
-            todos => {
-                re.send({
-                    todos
-                });
-            },
-            err => {
-                res.status(400).send(e);
-            }
-        );
+// app.post("/todos", (req, res) => {
+//     var todo = new Todo({
+//         text: req.body.text
+//     });
+//     todo.save().then(
+//         doc => {
+//             res.send(doc);
+//         },
+//         err => {
+//             res.status(400).send(err);
+//         }
+//     );
+// });
+
+// app.get("/todos", (req, res) => {
+//     Todo.find()
+//         .then(
+//             todos => {
+//                 re.send({
+//                     todos
+//                 });
+//             },
+//             err => {
+//                 res.status(400).send(e);
+//             }
+//         );
+// });
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    // valid id using is valid
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+    Todo.findById(id).then(todo => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send({
+            todo
+        });
+    }).catch(e => {
+        res.status(400).send();
+    });
+
 });
 
 app.listen(3000, () => {
